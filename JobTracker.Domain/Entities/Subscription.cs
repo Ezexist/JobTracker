438f93d2f73a380d08db445019ae1055fb4496e2
Ezexist <72872156+Ezexist@users.noreply.github.com>
2026-08-08 19:29:14 +0300

    Merge pull request #1 from Ezexist/feature/domain-entities
    feat:add BaseEntity

---
+++ JobTracker.Domain/Entities/Subscription.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Domain.Entities
+{
+    public class Subscription : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public bool RemoteOnly { get; set; }
+        public int? MinSalary{ get; set; }
+        public string? Currency { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        public ICollection<SubscriptionKeyword> Keywords { get; set; } = [];
+
+        public ICollection<SubscriptionLocation> Locations { get; set; } = [];
+
+        public ICollection<SubscriptionMatch> Matches { get; set; } = [];
+        public void Activate()
+        {
+            IsActive = true;
+        }
+
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+        public void Toggle()
+        {
+            IsActive = !IsActive;
+        }
+        public void Touch(DateTime utcNow)
+        {
+            UpdatedAt = utcNow;
+        }
+
+    }
+}
---
+++ JobTracker.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTracker.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public ICollection<Subscription> Subscriptions { get; set; } = [];
+    }
+}
